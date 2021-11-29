b4cab3b06d01422427dd93b028af097573ea3798
yuzer_naem <91341884+yuzer-naem@users.noreply.github.com>
2021-11-29 21:21:33 +0000

    initial commit

---
+++ bencoding.py
@@ -0,0 +1,51 @@
+class Encoder:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def encode(self) -> bytes:
+        return self._encode(self.obj)
+
+    @staticmethod
+    def _encode(obj) -> bytes:
+        if isinstance(obj, list):
+            return Encoder._encode_list(obj)
+        elif isinstance(obj, dict):
+            return Encoder._encode_dict(obj)
+        elif isinstance(obj, int):
+            return Encoder._encode_int(obj)
+        elif isinstance(obj, str):
+            return str(len(obj)).encode("ascii") + b":" + obj.encode("ascii")
+        elif isinstance(obj, bytes):
+            return str(len(obj)).encode("ascii") + b":" + obj
+        else:
+            raise ValueError("Bencoder got bad type: {}".format(type(obj)))
+
+    @staticmethod
+    def _encode_list(lst: list) -> bytes:
+        builder = b"l"
+
+        for item in lst:
+            builder += Encoder._encode(item)
+
+        builder += b"e"
+        return builder
+
+    @staticmethod
+    def _encode_dict(dct) -> bytes:
+        builder = b"d"
+
+        for key, value in dct.items():
+            builder += Encoder._encode(key)
+            builder += Encoder._encode(value)
+
+        builder += b"e"
+        return builder
+
+    @staticmethod
+    def _encode_int(num: int) -> bytes:
+        return ("i" + str(num) + "e").encode("ascii")
+
+
+class Decoder:
+    pass
+
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-file = input("enter torrent file to open.")
+from bencoding import Encoder
+
+file = input("enter torrent file to open: ")
 print(file)
+
+obj = {
+    "number": 5,
+    "list": ["we are gaming", 12, b"nice to meet you"]
+}
+
+encoder = Encoder(obj)
+
+print(encoder.encode())
