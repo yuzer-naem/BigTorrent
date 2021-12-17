ea5e3495611d17232dae9dbbc54efd764f5ca01e
yuzer_naem <91341884+yuzer-naem@users.noreply.github.com>
2021-12-17 17:12:19 +0000

    added unittests for the bencoding

---
+++ tests/bencoding_test.py
@@ -0,0 +1,23 @@
+import unittest
+from bdecode import Decoder
+from bencode import Encoder
+
+
+class TestBencoder(unittest.TestCase):
+    def setUp(self) -> None:
+        self.obj = {
+            b"number": 5,
+            b"list": [b"we are gaming", 12, b"nice to meet you"]
+        }
+
+        self.text = b"d6:numberi5e4:listl13:we are gamingi12e16:nice to meet youee"
+
+    def test_decode(self):
+        decoder = Decoder(self.text)
+        self.assertEqual(decoder.decode(), self.obj)
+
+    def test_encode(self):
+        encoder = Encoder(self.obj)
+        self.assertEqual(encoder.encode(), self.text)
+
+
